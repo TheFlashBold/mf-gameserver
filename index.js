316d97cc35a2b0db6052be8295edc62d423c52f7
tycho <tycho.holzer@gmail.com>
2020-04-14 21:44:17 +0200

    fixes

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const {model: GameServerModel} = require('./models/GameServer');
-const {Application, Module} = require('mf-lib');
-const path = require("path");
+const GameServerModule = require('./lib/GameServerModule');
 
-/**
- * @type GameServerModule
- */
-class GameServerModule extends Module {
-
-    async init() {
-        let test = await GameServerModel.findOne({name: 'test'});
-        if (!test) {
-            test = new GameServerModel({name: 'test', game: 'mcserver', path: path.resolve(Application.dirname, 'server', 'mcserver')});
-            await test.save();
-            await test.initLGSM();
-            await test.install();
-        } else {
-            await test.initLGSM();
-        }
-        test.start();
+module.exports = {
+    module: GameServerModule,
+    models: {
+        gameserver: require('./models/GameServer')
     }
-
-    async postInit() {
-
-    }
-}
-
-module.exports = new GameServerModule();
+};
